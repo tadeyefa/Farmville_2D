e9533d6f2b5f06b7e413eeba40b3ebbd4487adc2
tadeyefa <ohuntoluwase_adeyefa25@milton.edu>
2023-04-28 11:41:47 -0400

    Added 2D array.

--- src/MyFarm.java
+++ src/MyFarm.java
@@ -3,12 +3,13 @@ public class MyFarm {
         MyFarm emily = new MyFarm();
 
     }
+    public Plot[][] grid;
     public MyFarm() {
         System.out.println("Hello from my farm!");
         Plot leila = new Plot();
         leila.printPlot();
 
-        Plot[][] grid = new Plot[10][10];
+        grid = new Plot[4][3];
 
         for (int p=0;p<grid.length;p++) {
             for (int h=0;h<grid[p].length;h++) {
@@ -16,6 +17,11 @@ public class MyFarm {
                 grid[p][h].printPlot();
             }
         }
+        totalPlants();
+    }
+    public void totalPlants() {
+        // how many total plants are there in the grid?
+        System.out.println("There are " + (grid.length * grid.length) + " in the plot.");
     }
 
 }
