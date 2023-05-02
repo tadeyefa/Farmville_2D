8ad4c395d16bdad047b4529943ed3c9a63c5f5bd
tadeyefa <ohuntoluwase_adeyefa25@milton.edu>
2023-05-02 11:25:54 -0400

    Added two of the new methods from problem set.

--- src/MyFarm.java
+++ src/MyFarm.java
@@ -19,6 +19,9 @@ public class MyFarm {
         }
         totalPlants();
         printPlantNames();
+        totalCarrots();
+        averageNumberOfPlants();
+        numberOfTomatoPlots();
     }
     public void totalPlants() {
         // how many total plants are there in the grid?
@@ -37,5 +40,31 @@ public class MyFarm {
                 System.out.println("row: " + r + " col: " + c + " " + grid[r][c].plantName);
             }
         }
+    }
+    public void totalCarrots() {
+        // how many total carrot plants are there?
+        int sumOfCarrots = 0;
+        for (int h=0;h<grid.length;h++) {
+            for (int k=0;k<grid[h].length;k++) {
+                if (grid[h][k].plantName.equals("carrot")) {
+                    sumOfCarrots += grid[h][k].numberOfPlants;
+                }
+            }
+        }
+        System.out.println("The total amount of carrots is " + sumOfCarrots + " carrots");
+    }
+    public void averageNumberOfPlants() {
+        // what is the average number of plants across the whole plot?
+        int averagePlants = 0;
+        for (int p=0;p<grid.length;p++) {
+            for (int g=0;g<grid[p].length;g++) {
+                averagePlants += (grid[p][g].numberOfPlants);
+            }
+        }
+        System.out.println("The average of all the plants is " + averagePlants / (grid[0].length * grid.length));
+    }
+    public void numberOfTomatoPlots() {
+        // how many plots have tomatoes on them?
+
     }
 }
