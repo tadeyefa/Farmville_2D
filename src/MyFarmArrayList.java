1507d5768669b2dababeefb2fb88143c3717aabb
tadeyefa <ohuntoluwase_adeyefa25@milton.edu>
2023-05-16 11:23:47 -0400

    Learned about a for each loop

--- src/MyFarmArrayList.java
+++ src/MyFarmArrayList.java
@@ -5,21 +5,34 @@ public class MyFarmArrayList {
     public Plot[] bree = new Plot[10]; // array
     public MyFarmArrayList() {
         System.out.println("My farm using array list");
-//        bree[0] = new Plot();
-//        row.add(new Plot());
+
+//        for (Plot a : row) {
+//            System.out.println(row.size());
+//            row.add(new Plot());
+//            a.printPlot();
+//        }
 
         for (int p=0;p<10;p++) {
             System.out.println(row.size());
             row.add(new Plot());
             row.get(p).printPlot();
         }
-//        row.add(new Plot());
-//        System.out.println(row.size());
-
-        row.remove(0);
-        row.add(new Plot());
-        row.add(new Plot());
         row.add(new Plot());
         System.out.println(row.size());
+
+//        row.remove(0);
+//        row.add(new Plot());
+//        row.add(new Plot());
+//        row.add(new Plot());
+//        System.out.println(row.size());
+        totalPlants();
+    }
+    public void totalPlants() {
+        // how many total plants are there in the row?
+        int total = 0;
+        for (Plot a : row) {
+            total += a.numberOfPlants;
+        }
+        System.out.println("There are " + total + " plants in total");
     }
 }
