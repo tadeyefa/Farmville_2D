4b6ec8568df4cbcddbb1ce19c2c2801998cd076b
tadeyefa <ohuntoluwase_adeyefa25@milton.edu>
2023-05-19 13:08:29 -0400

    Added more plots to the array list and used the row.add() method more.

--- src/MyFarmArrayList.java
+++ src/MyFarmArrayList.java
@@ -26,6 +26,11 @@ public class MyFarmArrayList {
 //        row.add(new Plot());
 //        System.out.println(row.size());
         totalPlants();
+        printPlantNames();
+        addSunflower();
+        printPlantNames();
+        addCorn();
+        printPlantNames();
     }
     public void totalPlants() {
         // how many total plants are there in the row?
@@ -35,4 +40,32 @@ public class MyFarmArrayList {
         }
         System.out.println("There are " + total + " plants in total");
     }
+    public void printPlantNames() {
+        // print the name of each plant in the row and its index
+        for (int k=0;k<row.size();k++) {
+            System.out.print(k + ": " + row.get(k).plantName + " \t");
+        }
+        System.out.println();
+    }
+    public void addSunflower() {
+        // add a sunflower plot to the end of the row with 42 plants that need water
+        // call printPlantNames() method before and after your addition to make sure it worked
+        Plot sunflower = new Plot();
+        sunflower.plantName = "sunflower";
+        sunflower.needsWater = true;
+        sunflower.numberOfPlants = 42;
+        row.add(sunflower);
+        sunflower.printPlot();
+    }
+    public void addCorn() {
+        // at index 3, add a new corn plot with 70 plants that do not need water
+        // call printPlantNames() method before and after your addition to make sure it worked
+        Plot corn = new Plot();
+        corn.plantName = "corn";
+        corn.numberOfPlants = 70;
+        corn.needsWater = false;
+        row.add(3, corn);
+        corn.printPlot();
+    }
+
 }
